779782979d8db279fdbe6805e8426ada0e7af520
Mark Ogden <mark.pm.ogden@btinternet.com>
2023-10-11 10:40:40 +0100

    2023.10.11.3: added license info

--- showVersion.h
+++ showVersion.h
@@ -1,3 +1,23 @@
+/* mklbr - create .lbr archives from recipes
+ * Copyright (C) - 2020-2023 Mark Ogden
+ *
+ * showVersion.h - macro interface to showVersion information function
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
 /* showVersion.h: _REVISION_ */
 #ifndef _SHOWVERSION_H_
 #define _SHOWVERSION_H_
